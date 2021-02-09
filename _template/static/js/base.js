f1d6f31ccdc02c17456d4c6a81aab79355b8c622
Denis <qwertyuiop852002058@yandex.ru>
2021-02-09 19:25:45 +0300

    ...

--- _template/static/js/base.js
+++ _template/static/js/base.js
@@ -4,7 +4,6 @@ const options = window.location.search.replace('?', '').split('&')
         p[decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
         return p;
     }, {});
-options.slice = 10;
 moment.locale("ru");
 DB.getTypes().then(types => {
     links = types.filter(type => type.page).map(type => `<li class="nav-item"><a class="nav-link"href="{{ start_url }}/page.html?page=page&type=${type.uuid}">${type.page.title}</a></li>`);
