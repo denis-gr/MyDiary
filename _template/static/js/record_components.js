8517981efdd9787eda93196aa6051e0d4048fb71
Denis <qwertyuiop852002058@yandex.ru>
2021-02-13 15:02:03 +0300

    ....

--- _template/static/js/record_components.js
+++ _template/static/js/record_components.js
@@ -1,12 +1,12 @@
-const getCrearionMenuLinkTemplate = type => document.querySelector("#record-modal-template").innerHTML.replaceAll("{type_name}", type.name).replaceAll("{type_icon}", type.icon).replaceAll("{type_icon}", type.icon);
+const getModalRecordHTML = type => document.querySelector("#record-modal-template").innerHTML.replace("{form}", type.form_template);
+const getCrearionMenuLinkTemplate = type => document.querySelector("#creation-menu-link-template").innerHTML.replaceAll("{type_name}", type.name).replaceAll("{type_icon}", type.icon).replaceAll("{type_icon}", type.icon);
 const getCrearionElement = type => document.querySelector("#creation-element-template").innerHTML.replaceAll("{type_name}", type.name).replaceAll("{type_icon}", type.icon).replaceAll("{type_title}", type.title);
 const getRecordHTML = type => document.querySelector("#record-template").innerHTML.replaceAll("{type_name}", type.name).replaceAll("{type_icon}", type.icon).replaceAll("{type_template}", type.template);
 
 function createVueModalRecord(type) {
     Vue.component(`${type.name}-modal-record`, {
         props: ["record"],
-        template: document.querySelector("#record-modal-template")
-            .innerHTML.replace("{form}", type.form_template),
+        template: getModalRecordHTML(type),
     });
 };
 
