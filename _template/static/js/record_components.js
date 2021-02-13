2df182a81379d91b5251720d5dea428ae2ead69d
Denis <qwertyuiop852002058@yandex.ru>
2021-02-13 14:51:56 +0300

    ...

--- _template/static/js/record_components.js
+++ _template/static/js/record_components.js
@@ -1,3 +1,7 @@
+const getCrearionMenuLinkTemplate = type => document.querySelector("#record-modal-template").innerHTML.replaceAll("{type_name}", type.name).replaceAll("{type_icon}", type.icon).replaceAll("{type_icon}", type.icon);
+const getCrearionElement = type => document.querySelector("#creation-element-template").innerHTML.replaceAll("{type_name}", type.name).replaceAll("{type_icon}", type.icon);
+const getRecordHTML = type => document.querySelector("#record-template").innerHTML.replaceAll("{type_name}", type.name).replaceAll("{type_icon}", type.icon).replaceAll("{type_template}", type.template);
+
 function createVueModalRecord(type) {
     Vue.component(`${type.name}-modal-record`, {
         props: ["record"],
@@ -5,18 +9,6 @@ function createVueModalRecord(type) {
             .innerHTML.replace("{form}", type.form_template),
     });
 };
-
-typesModal = {};
-DB.getTypes().then(recordTypes => {
-    recordTypes.forEach(type => {
-        typesModal[type.uuid] = `${type.name}-modal-record`;
-        createVueModalRecord(type);
-    });
-    window.getModalComponent = uuid => typesModal[uuid];
-});
-
-const getCrearionMenuLinkTemplate = type => `<button class="btn btn-outline-secondary link"type="button"data-bs-toggle="modal"data-bs-target="#add-${type.name}"><i class="bi-${type.icon} icon"title="Создать ${type.title}"></i></button>`;
-const getCrearionElement = type => `<modal title="${type.title}"id="add-${type.name}"><component is="${type.name}-modal-record" :record="record"></component><template v-slot:footer><button type="button"class="btn btn-primary"data-bs-dismiss="modal"@click="add(record)">Создать</button></template> </modal>`;
 
 function toggleCreationMenu() {
     document.querySelector("#creation-menu").classList.toggle("show")
@@ -60,17 +52,6 @@ function createVueCreate(b) {
         },
     })
 };
-
-DB.getTypes().then(recordTypes => {
-    templates = recordTypes.map(getCrearionMenuLinkTemplate).join("");
-    document.querySelector("#creation-menu-inner").insertAdjacentHTML('afterbegin', templates);
-    recordTypes.forEach(type => {
-        template = getCrearionElement(type);
-        document.querySelector("#creation-menu").insertAdjacentHTML('beforebegin', template);
-        createVueCreate(type);
-    });
-});
-const getRecordHTML = type => `<div class="record-preview"><modal title="Изменить запись" :id="'editRecord'+id_record"><component is="${type.name}-modal-record" :record="form"></component><template v-slot:footer><button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="remove()">Удалить</button><button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="save()">Изменить</button></template></modal><i class="bi-${type.icon} icon left"></i><div class="right"><div class="content" data-bs-toggle="modal" :data-bs-target="'#editRecord'+id_record">${type.template}</div><div class="bottom"><a class="date" v-text="datetime(record)" :href="url_date(record)"></a><a class="tag" v-for="tag in record.tags" v-text="'#'+tag" :href="url_tag(tag)"></a></div></div></div>`;
 
 function createVueRecord(a, b) {
     Vue.component(`${a.name}-record`, {
@@ -120,11 +101,26 @@ function createVueRecord(a, b) {
     })
 };
 
+typesModal = {};
 types = {};
 DB.getTypes().then(recordTypes => {
+    recordTypes.forEach(type => {
+        typesModal[type.uuid] = `${type.name}-modal-record`;
+        createVueModalRecord(type);
+    });
+    window.getModalComponent = uuid => typesModal[uuid];
+
+    templates = recordTypes.map(getCrearionMenuLinkTemplate).join("");
+    document.querySelector("#creation-menu-inner").insertAdjacentHTML('afterbegin', templates);
+    recordTypes.forEach(type => {
+        template = getCrearionElement(type);
+        document.querySelector("#creation-menu").insertAdjacentHTML('beforebegin', template);
+        createVueCreate(type);
+    });
+
     recordTypes.forEach(type => {
         types[type.uuid] = `${type.name}-record`;
         createVueRecord(type, recordTypes);
     });
     window.getComponent = uuid => types[uuid];
-})
+});
